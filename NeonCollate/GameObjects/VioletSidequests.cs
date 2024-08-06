e689b2e0ecb417b9be7f267100f4ea55b0dff8e2
DerelictJade <derelictjade27@gmail.com>
2024-08-06 18:38:40 +1200

    Add project files.

---
+++ NeonCollate/GameObjects/VioletSidequests.cs
@@ -0,0 +1,70 @@
+﻿using MelonLoader;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace NeonCollate.GameObjects
+{
+    internal class VioletSidequests
+    {
+        private static GameObject _violetSidequests;
+        private static GameObject _mainMenu;
+
+        public static void PushDown()
+        {
+            if (_violetSidequests != null)
+                _violetSidequests.transform.SetAsLastSibling();
+        }
+
+        public static void Click()
+        {
+            _mainMenu.GetComponent<MainMenu>().SelectMission("M_SIDEQUESTS_VIOLET", true, false);
+        }
+
+        internal static void Initialize()
+        {
+            if (_violetSidequests != null)
+                return;
+
+            _mainMenu = GameObject.Find("Main Menu");
+            GameObject missionButton = GameObject.Find("Main Menu/Canvas/Main Menu/Panel/Missions Panel/Scroll View/Viewport/Buttons Holder/Mission Button Holder");
+
+
+            if (_mainMenu == null || missionButton == null)
+            {
+                MelonLogger.Msg("Houston, we have a problem");
+                return;
+            }
+
+            _violetSidequests = GameObject.Instantiate(missionButton, missionButton.transform.parent);
+            _violetSidequests.name = "Violet Sidequests Mission Button";
+
+            // Disable animator to prevent hiding
+            _violetSidequests.GetComponent<Animator>().enabled = false;
+
+            Button _button = _violetSidequests.GetComponentInChildren<Button>();
+
+            ColorBlock _buttonColors = _button.colors;
+            _buttonColors.normalColor = new Color(1f, 1f, 1f, 1f);
+            _buttonColors.highlightedColor = new Color(0.5074f, 0.9782f, 1f, 1f);
+            _buttonColors.pressedColor = new Color(0.0077f, 0.5221f, 0.4999f, 1f);
+            _buttonColors.selectedColor = new Color(0.5074f, 0.9782f, 1f, 1f);
+            _buttonColors.disabledColor = new Color(0.7843f, 0.7843f, 0.7843f, 0.502f);
+            _button.colors = _buttonColors;
+            
+            _violetSidequests.GetComponentInChildren<Image>().color = new Color(1f, 1, 1f, 1f);
+
+            TextMeshProUGUI text = _violetSidequests.transform.Find("Button").GetComponentInChildren<TextMeshProUGUI>();
+            text.SetText("Violet Sidequests");
+            text.color = new Color(0f, 0f, 0f, 1f);
+
+            TextMeshProUGUI counterText = _violetSidequests.transform.Find("Button").transform.Find("CounterText").GetComponentInChildren<TextMeshProUGUI>();
+            counterText.SetText("♡");
+            //counterText.SetText("V");
+            counterText.color = new Color(0.502f, 0.502f, 0.502f, 1f);
+
+            _button.onClick.AddListener(Click);
+
+        }
+    }
+}
---
+++ NeonCollate/NeonCollate.cs
@@ -0,0 +1,78 @@
+﻿using MelonLoader;
+using NeonCollate.GameObjects;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using HarmonyLib;
+using System.Security.Cryptography;
+
+namespace NeonCollate
+{
+    public class NeonCollate : MelonMod
+    {
+        internal static new HarmonyLib.Harmony Harmony { get; private set; }
+        public static void PushButtonsDown()
+        {
+            RedSidequests.PushDown();
+            VioletSidequests.PushDown();
+            YellowSidequests.PushDown();
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            base.OnLateInitializeMelon();
+
+            // Initialize Harmony
+            Harmony = HarmonyInstance;
+            Harmony.PatchAll();
+
+            // Other initialization code
+        }
+        public override void OnSceneWasLoaded(int buildindex, string sceneName)
+        {
+            if (sceneName.Equals("Heaven_Environment") || sceneName.Equals("HUB_HEAVEN"))
+            {
+                GoToJobArchive.Initialize();
+                GoToJobArchive.Hide();
+
+                RedSidequests.Initialize();
+                VioletSidequests.Initialize();
+                YellowSidequests.Initialize();
+                return;
+            }
+        }
+        public override void OnApplicationQuit()
+        {
+            Harmony.UnpatchAll();
+            base.OnApplicationQuit();
+        }
+
+        [HarmonyPatch(typeof(MainMenu), "SetState")]
+        public class MainMenuSetStatePatch
+        {
+            [HarmonyPostfix]
+            static void Postfix()
+            {
+                if (!GameObject.Find("Main Menu") || !GameObject.Find("Main Menu").GetComponent<MainMenu>())
+                    return;
+
+                LocationData currentLocation = GameObject.Find("Main Menu").GetComponent<MainMenu>().GetCurrentLocation();
+
+                if (currentLocation && currentLocation.name == "Location_WhitesRoom")
+                    GoToJobArchive.Show();
+                else
+                    GoToJobArchive.Hide();
+            }
+        }
+
+        [HarmonyPatch(typeof(MainMenu), "SelectCampaign")]
+        public class MainMenuSelectCampaignPatch
+        {
+            [HarmonyPostfix]
+            static void Postfix()
+            {
+                PushButtonsDown();
+            }
+        }
+
+    }
+}
