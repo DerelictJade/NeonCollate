29940ebffffaf1a22b56e1523df54adca415e93c
DerelictJade <derelictjade27@gmail.com>
2024-08-06 18:55:23 +1200

    Initial commit

--- NeonCollate/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using MelonLoader;
-using NeonCollate;
-
-[assembly: MelonInfo(typeof(NeonCollate.NeonCollate), "NeonCollate", "0.1.0", "DerelictJade")]
-[assembly: MelonColor(255, 196, 112, 253)]
-[assembly: MelonAuthorColor(255, 235, 215, 255)]
